8650b5f48d28eb88b079f01aa86927d3d349ff7c
rafiqcoder <rafiqcoder@gmail.com>
2023-12-27 00:03:54 +0600

    fixed route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { userRoutes } from './app/modules/user/user.route';
+import { userRouter } from './app/modules/user/user.route';
+
 const app :Application = express();
 
 
@@ -8,7 +9,7 @@ const app :Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/api', userRoutes);
+app.use('/api', userRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,26 +1,26 @@
-import express, { Router } from 'express';
-import validateRequest from '../../middleware/validateRequest';
-import { userController } from './user.controller';
-import { validateUserSchema } from './user.validation';
-const router: Router = express.Router();
+import express, { Router } from "express"
+import validateRequest from "../../middleware/validateRequest"
+import { userController } from "./user.controller"
+import { validateUserSchema } from "./user.validation"
+const router: Router = express.Router()
 
 router.post(
-  '/users',
+  "/users",
   validateRequest(validateUserSchema.createUserSchemaValidation),
   userController.createUser,
-);
+)
 
-router.get('/users', userController.getAllUser);
+router.get("/users", userController.getAllUser)
 
-router.get('/users/:userId', userController.getUser);
+router.get("/users/:userId", userController.getUser)
 
 router.put(
-  '/users/:userId',
+  "/users/:userId",
   validateRequest(validateUserSchema.updateUserSchemaValidation),
   userController.updateOneUser,
-);
-router.delete('/users/:userId', userController.deleteOneUser);
+)
+router.delete("/users/:userId", userController.deleteOneUser)
 
-router.put('/users/:userId/orders', userController.createOrder);
+router.put("/users/:userId/orders", userController.createOrder)
 
-export const userRouter = router;
+export const userRouter = router
